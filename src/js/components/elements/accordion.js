893dc5becb881872e121bf6d68e6ff29e0cb4bf4
Arth Raval <connect.arth.raval@gmail.com>
2022-07-20 19:49:46 +0530

    test over

--- src/js/components/elements/accordion.js
+++ src/js/components/elements/accordion.js
@@ -1,1 +1,77 @@
-const collapseControl=(el)=>{console.log(el.parentNode.parentNode.childNodes); $(el.parentNode.parentNode.childNodes[3]).css("transition", "0.15s ease-out"); if ($(el.parentNode.parentNode.childNodes[3]).hasClass("collapse")){$(el).css("transform", "rotate(180deg)"); $(el.parentNode.parentNode.childNodes[3]).removeClass("collapse");}else{$(el).css("transform", "rotate(0deg)"); $(el.parentNode.parentNode.childNodes[3]).addClass("collapse");}};const accordion=()=>{let color=""; let cup_item=""; array=[...primary]; cup_copy=[...cupcake]; array=array.sort(()=> Math.random() - 0.5); cup_copy=cup_copy.sort(()=> Math.random() - 0.5); $(".generateCanavas").html(""); $(".titleCanavas").html(` <h1 class="pb-2 mb-2 text-xl font-bold border-b">Accordion</h1> <p>The accordion component allows the user to show and hide sections of related content on a page</p>`); for (let index=0; index < 22; index++){color=array.pop(); // cup_item=cup_copy.pop(); $(".generateCanavas").append(` <div class="w-full accordion"> <div class="grid grid-cols-1 gap-4 text-justify text-${color}-900"> <div class="flex items-center justify-between px-5 py-3 bg-white rounded-lg shadow"> <div class="flex items-center gap-4"> <div class="w-5 h-5 bg-${color}-50 rounded-full"></div><div class="w-5 h-5 bg-${color}-100 rounded-full"></div><div class="w-5 h-5 bg-${color}-300 rounded-full"></div><div class="w-5 h-5 bg-${color}-900 rounded-full"></div></div><div onclick="copyToClipBoard(this)" class="text-${color}-900 px-2 bg-${color}-100 rounded-full"> copy </div></div><div class="shadow-md bg-white rounded-lg shadow-${color}-200"> <div class="px-5 py-3 "> <div class="flex items-center justify-between"> <p class="py-1 text-lg md:text-xl font-semibold">Collapsible Grop 1 </p><img onclick="collapseControl(this)" src="src/img/arrow.svg" class="w-6 p-0.5 rounded-full bg-${color}-100" alt=""/> </div><div id="collapse" class="collapse"> <p class="text-${color}-900/90">${cup_copy[Math.floor(Math.random() * 22)]}</p></div></div><div class="px-5 py-3 "> <div class="flex items-center justify-between"> <p class="py-1 text-lg md:text-xl font-semibold">Collapsible Grop 2 </p><img onclick="collapseControl(this)" src="src/img/arrow.svg" class="w-6 p-0.5 rounded-full bg-${color}-100" alt=""/> </div><div id="collapse" class="collapse"> <p class="text-${color}-900/90">${cup_copy[Math.floor(Math.random() * 22)]}</p></div></div><div class="px-5 py-3 "> <div class="flex items-center justify-between"> <p class="py-1 text-lg md:text-xl font-semibold">Collapsible Grop 3 </p><img onclick="collapseControl(this)" src="src/img/arrow.svg" class="w-6 p-0.5 rounded-full bg-${color}-100" alt=""/> </div><div id="collapse" class="collapse"> <p class="text-${color}-900/90">${cup_copy[Math.floor(Math.random() * 22)]}</p></div></div></div></div></div>`);}};
+const collapseControl = (el) => {
+  console.log(el.parentNode.parentNode.childNodes);
+  $(el.parentNode.parentNode.childNodes[3]).css("transition", "0.15s ease-out");
+  if ($(el.parentNode.parentNode.childNodes[3]).hasClass("collapse")) {
+    $(el).css("transform", "rotate(180deg)");
+    $(el.parentNode.parentNode.childNodes[3]).removeClass("collapse");
+  } else {
+    $(el).css("transform", "rotate(0deg)");
+    $(el.parentNode.parentNode.childNodes[3]).addClass("collapse");
+  }
+};
+const accordion = () => {
+  let color = "";
+  let cup_item = "";
+  array = [...primary];
+  cup_copy = [...cupcake];
+  array = array.sort(() => Math.random() - 0.5);
+  cup_copy = cup_copy.sort(() => Math.random() - 0.5);
+  $(".generateCanavas").html("");
+  $(".titleCanavas").html(`
+  <h1 class="pb-2 mb-2 text-xl font-bold border-b">Accordion</h1>
+  <p>The accordion component allows the user to show and hide sections of related content on a page</p>
+  `);
+  for (let index = 0; index < 22; index++) {
+    color = array.pop();
+    // cup_item = cup_copy.pop();
+    $(".generateCanavas").append(` <div
+    class="w-full accordion">
+    <div class="grid grid-cols-1 gap-4 text-justify text-${color}-900">
+        <div class="flex items-center justify-between px-5 py-3 bg-white rounded-lg shadow">
+            <div class="flex items-center gap-4">
+                <div class="w-5 h-5 bg-${color}-50 rounded-full"></div>
+                <div class="w-5 h-5 bg-${color}-100 rounded-full"></div>
+                <div class="w-5 h-5 bg-${color}-300 rounded-full"></div>
+                <div class="w-5 h-5 bg-${color}-900 rounded-full"></div>
+            </div>
+            <div onclick="copyToClipBoard(this)" class="text-${color}-900 px-2 bg-${color}-100 rounded-full">
+                copy
+            </div>
+        </div>
+        <div class="shadow-md bg-white rounded-lg shadow-${color}-200">
+            <div class="px-5 py-3 ">
+                <div class="flex items-center justify-between">
+                    <p class="py-1 text-lg md:text-xl font-semibold">Collapsible Grop 1 </p>
+                    <img onclick="collapseControl(this)" src="src/img/arrow.svg" class="w-6 p-0.5 rounded-full bg-${color}-100" alt="" />
+                </div>
+                <div id="collapse" class="collapse">
+                    <p class="text-${color}-900/90">${cup_copy[Math.floor(Math.random() * 22)]}
+                    </p>
+                </div>
+            </div>
+            <div class="px-5 py-3 ">
+                <div class="flex items-center justify-between">
+                    <p class="py-1 text-lg md:text-xl font-semibold">Collapsible Grop 2 </p>
+                    <img onclick="collapseControl(this)" src="src/img/arrow.svg" class="w-6 p-0.5 rounded-full bg-${color}-100" alt="" />
+                </div>
+                <div id="collapse" class="collapse">
+                    <p class="text-${color}-900/90">${cup_copy[Math.floor(Math.random() * 22)]}
+                    </p>
+                </div>
+            </div>
+            <div class="px-5 py-3 ">
+                <div class="flex items-center justify-between">
+                    <p class="py-1 text-lg md:text-xl font-semibold">Collapsible Grop 3 </p>
+                    <img onclick="collapseControl(this)" src="src/img/arrow.svg" class="w-6 p-0.5 rounded-full bg-${color}-100" alt="" />
+                </div>
+                <div id="collapse" class="collapse">
+                    <p class="text-${color}-900/90">${cup_copy[Math.floor(Math.random() * 22)]}
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    `);
+  }
+};
--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,1 +1,91 @@
-const selectSidenavItem=(el)=>{$(el.parentNode.querySelector(".active-sidenav-item")).removeClass( "text-slate-900 bg-slate-100 active-sidenav-item" ); $(el).addClass("text-slate-900 bg-slate-100 active-sidenav-item"); switchTab(el.id); if ($(window).width() < 768){$("#sidebar").removeClass("open"); $("#sidebar").removeClass("fixed");}};const sidebarComponent=()=>{return ` <nav id="sidebar" class="sidebar noselect px-4 text-slate-500 my-2 md:my-4 py-2 h-screen md:overflow-y-scroll bg-white rounded-r-lg shadow"> <div> <div class="text-lg"> <div> <div class="mb-2 pb-2 text-xl font-semibold text-slate-700 border-b flex items-center justify-between"> <h2 class="">Elements</h2> <img onclick="collapseControl(this)" src="src/img/arrow.svg" class="w-6 p-0.5 rounded-full bg-slate-100" alt="" style="transform: rotate(180deg);"/> </div><div id="collapse" class="flex flex-col gap-1"> <p id='1' onclick="selectSidenavItem(this)" class='sidenav-item px-2 py-1 text-slate-900 bg-slate-100 hover:bg-slate-100 hover:text-slate-900 rounded-lg active-sidenav-item'> Accordion </p><p id='2' onclick="selectSidenavItem(this)" class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Avatars </p><p onclick="selectSidenavItem(this)" class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Dropdowns </p><p onclick="selectSidenavItem(this)" class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Badges </p><p onclick="selectSidenavItem(this)" class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Buttons </p><p onclick="selectSidenavItem(this)" class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Button Groups </p></div></div><div> <div class="mt-5 mb-2 pb-2 text-xl font-semibold text-slate-700 border-b flex items-center justify-between"> <h2>Navigation</h2> <img onclick="collapseControl(this)" src="src/img/arrow.svg" class="w-6 p-0.5 rounded-full bg-slate-100" alt="" style="transform: rotate(180deg);"/> </div><div class="flex flex-col gap-1"> <p onclick="selectSidenavItem(this)" class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Navbars </p><p onclick="selectSidenavItem(this)" class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Pagination </p><p onclick="selectSidenavItem(this)" class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Tabs</p><p onclick="selectSidenavItem(this)" class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Vertical Navigation </p><p onclick="selectSidenavItem(this)" class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Sidebar Navigation </p><p onclick="selectSidenavItem(this)" class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'> Breadcrumbs </p><p onclick="selectSidenavItem(this)" class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Steps </p><p onclick="selectSidenavItem(this)" class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Command Palettes </p></div></div></div></div></nav> `;};
+const selectSidenavItem = (el) => {
+  $(el.parentNode.querySelector(".active-sidenav-item")).removeClass(
+    "text-slate-900 bg-slate-100 active-sidenav-item"
+  );
+  $(el).addClass("text-slate-900 bg-slate-100 active-sidenav-item");
+  switchTab(el.id);
+
+  if ($(window).width() < 768) {
+    $("#sidebar").removeClass("open");
+    $("#sidebar").removeClass("fixed");
+  }
+};
+const sidebarComponent = () => {
+  return `
+    <nav id="sidebar"
+        class="sidebar noselect px-4 text-slate-500 my-2 md:my-4 py-2 h-screen md:overflow-y-scroll bg-white rounded-r-lg shadow">
+        <div>
+            <div class="text-lg">
+                <div>
+                    <div
+                        class="mb-2 pb-2 text-xl font-semibold text-slate-700 border-b flex items-center justify-between">
+                        <h2 class="">Elements</h2>
+                        <img onclick="collapseControl(this)" src="src/img/arrow.svg"
+                            class="w-6 p-0.5 rounded-full bg-slate-100" alt="" style="transform: rotate(180deg);" />
+                    </div>
+                    <div id="collapse" class="flex flex-col gap-1">
+                        <p id='1' onclick="selectSidenavItem(this)"
+                            class='sidenav-item px-2 py-1 text-slate-900 bg-slate-100 hover:bg-slate-100 hover:text-slate-900 rounded-lg active-sidenav-item'>
+                            Accordion
+                        </p>
+                        <p id='2' onclick="selectSidenavItem(this)"
+                            class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Avatars
+                        </p>
+                        <p onclick="selectSidenavItem(this)"
+                            class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Dropdowns
+                        </p>
+                        <p onclick="selectSidenavItem(this)"
+                            class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Badges
+                        </p>
+                        <p onclick="selectSidenavItem(this)"
+                            class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Buttons
+                        </p>
+                        <p onclick="selectSidenavItem(this)"
+                            class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Button
+                            Groups
+                        </p>
+                    </div>
+                </div>
+
+                <div>
+                    <div
+                        class="mt-5 mb-2 pb-2 text-xl font-semibold text-slate-700 border-b flex items-center justify-between">
+                        <h2>Navigation</h2>
+                        <img onclick="collapseControl(this)" src="src/img/arrow.svg"
+                            class="w-6 p-0.5 rounded-full bg-slate-100" alt="" style="transform: rotate(180deg);" />
+                    </div>
+
+                    <div class="flex flex-col gap-1">
+                        <p onclick="selectSidenavItem(this)"
+                            class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Navbars
+                        </p>
+                        <p onclick="selectSidenavItem(this)"
+                            class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Pagination
+                        </p>
+                        <p onclick="selectSidenavItem(this)"
+                            class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Tabs</p>
+                        <p onclick="selectSidenavItem(this)"
+                            class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Vertical
+                            Navigation
+                        </p>
+                        <p onclick="selectSidenavItem(this)"
+                            class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Sidebar
+                            Navigation
+                        </p>
+                        <p onclick="selectSidenavItem(this)"
+                            class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>
+                            Breadcrumbs
+                        </p>
+                        <p onclick="selectSidenavItem(this)"
+                            class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Steps </p>
+                        <p onclick="selectSidenavItem(this)"
+                            class='sidenav-item px-2 py-1 hover:bg-slate-100 hover:text-slate-900 rounded-lg'>Command
+                            Palettes
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    `;
+};
